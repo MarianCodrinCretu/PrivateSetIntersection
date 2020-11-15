92b8f12445aceaee94c17e0233def037185b7155
Cretu Marian-Codrin <cretu.marian.codrin@gmail.com>
2020-11-15 22:23:33 +0200

    [PSI-21] Added loggin g system for Communication class

--- CommunicationService/TransferProtocol.py
+++ CommunicationService/TransferProtocol.py
@@ -1,3 +1,11 @@
+import csv
+import datetime
+from datetime import date
+
+import aspectlib
+from aspectlib import Aspect
+import Logs.LogMessaging
+
 from CommunicationService.ComReceive import ComReceive
 from CommunicationService.ComSend import ComSend
 import CryptoUtils.CryptoUtils
@@ -16,70 +24,136 @@ class TransferProtocol:
         self._comSend = comSend
         self._comReceive = comReceive
 
+    @Aspect
+    def log_results(self, message):
+        data = yield aspectlib.Proceed
+        if message[1]=='SENDER':
+            file='communicationLogsSender.csv'
+        else:
+            file = 'communicationLogsReceiver.csv'
+        with open(file, mode='a') as log:
+            log_writer = csv.writer(log, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            log_writer.writerow([message[0], message[1], message[2], message[3]])
+
+    def processMessage(self, message):
+        print("%s--%s--%s--%s"%(message[0], message[1], message[2], message[3]))
+
     # function for sending parameters in negotiation process
     # @param paramsDictionary the dictionary of negotiation parameters
     # {'lambda': int, 'sigma':int, 'm':int, 'w':int, 'l1':int, 'l2':int, 'hash1':string, 'hash2':string, 'PRF':string}
 
     def initiateConnection(self):
         self._comSend.send("Connection attempting!", self._connectionParams['Server IP'],
-                           int(self._connectionParams['Server Port']),
-                           HEADERSIZE=10, flag='NoAES')
+                       int(self._connectionParams['Server Port']),
+                       HEADERSIZE=10, flag='NoAES')
+        message = Logs.LogMessaging.createLogInitiateMessage(self._connectionParams['Server IP'],
+                       self._connectionParams['Server Port'])
+
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+
+
+
 
     def sendConfirmationInitiateConnection(self):
         self._comSend.send("Received connection attempting! All ok!", self._connectionParams['Client IP'],
                            int(self._connectionParams['Client Port']),
                            HEADERSIZE=10, flag='NoAES')
+        message = Logs.LogMessaging.createLogSendConfirmationMessage(self._connectionParams['Client IP'],
+                           self._connectionParams['Client Port'], self._connectionParams['Server IP'],
+                                                         self._connectionParams['Server Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+
+
 
     def sendRSAReceiverPublicKey(self, key):
+
         key = CryptoUtils.CryptoUtils.convertRSAKeyToString(key)
         self._comSend.send(key, self._connectionParams['Server IP'],
                            int(self._connectionParams['Server Port']),
                            HEADERSIZE=50, flag='NoAES')
+        message = Logs.LogMessaging.createLogSendReceiverRSA(self._connectionParams['Server IP'],
+                       self._connectionParams['Server Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
 
     def receiveRSAReceiverPublicKey(self):
 
         result= self._comReceive.receive(self._connectionParams['Server IP'], int(self._connectionParams['Server Port']),
                                         HEADERSIZE=50, flag='NoAES')
+        message = Logs.LogMessaging.createLogReceiveReceiverRSA(self._connectionParams['Client IP'],
+                       self._connectionParams['Client Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
         return CryptoUtils.CryptoUtils.stringToRSAKey(result)
 
+
     def sendRSASenderPublicKey(self, key):
+
         key = CryptoUtils.CryptoUtils.convertRSAKeyToString(key)
         self._comSend.send(key, self._connectionParams['Client IP'],
                            int(self._connectionParams['Client Port']),
                            HEADERSIZE=50, flag='NoAES')
+        message = Logs.LogMessaging.createLogSendSenderRSA(self._connectionParams['Client IP'],
+                       self._connectionParams['Client Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
 
 
     def receiveRSASenderPublicKey(self):
+
         result = self._comReceive.receive(self._connectionParams['Client IP'], int(self._connectionParams['Client Port']),
                                         HEADERSIZE=50, flag='NoAES')
+        message = Logs.LogMessaging.createLogReceiveReceiverRSA(self._connectionParams['Server IP'],
+                       self._connectionParams['Server Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
         return CryptoUtils.CryptoUtils.stringToRSAKey(result)
 
 
 
+
     def sendIVByRSA(self, iv, rsaKey):
+
         iv = CryptoUtils.CryptoUtils.rsaEncrypt(rsaKey, iv)
         self._comSend.send(iv, self._connectionParams['Server IP'],
                            int(self._connectionParams['Server Port']),
                            HEADERSIZE=10, flag='NoAES')
+        message = Logs.LogMessaging.createLogSendIV(self._connectionParams['Server IP'],
+                                                                          self._connectionParams['Server Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+
 
     def receiveIVByRSA(self, rsaKey):
-        return CryptoUtils.CryptoUtils.rsaDecrypt(
-            rsaKey, self._comReceive.receive(self._connectionParams['Server IP'],  int(self._connectionParams['Server Port']),
-                                        HEADERSIZE=10, flag='NoAES'))
+
+        result = self._comReceive.receive(self._connectionParams['Server IP'],  int(self._connectionParams['Server Port']),
+                                    HEADERSIZE=10, flag='NoAES')
+        message = Logs.LogMessaging.createLogReceiveIV(self._connectionParams['Client IP'],  self._connectionParams['Client Port'])
+
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+        return CryptoUtils.CryptoUtils.rsaDecrypt(rsaKey, result)
 
     def sendAESKeyByRSA(self, aesKey, rsaKey):
         aesKey = CryptoUtils.CryptoUtils.rsaEncrypt(rsaKey, aesKey)
         self._comSend.send(aesKey, self._connectionParams['Client IP'],
                            int(self._connectionParams['Client Port']),
                            HEADERSIZE=10, flag='NoAES')
+        message = Logs.LogMessaging.createLogSendKey(self._connectionParams['Client IP'],
+                                                              self._connectionParams['Client Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
 
     def receiveAESKeyByRSA(self, rsaKey):
-        return CryptoUtils.CryptoUtils.rsaDecrypt(rsaKey,
-                                                  self._comReceive.receive
-                                                  (self._connectionParams['Client IP'], int(self._connectionParams['Client Port']),
-                                        HEADERSIZE=10, flag='NoAES'))
-
-
+        result = self._comReceive.receive (self._connectionParams['Client IP'], int(self._connectionParams['Client Port']),
+                                        HEADERSIZE=10, flag='NoAES')
+        message = Logs.LogMessaging.createLogReceiveIV(self._connectionParams['Server IP'],
+                                                       self._connectionParams['Server Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+        return CryptoUtils.CryptoUtils.rsaDecrypt(rsaKey, result)
 
 
 
@@ -89,43 +163,95 @@ class TransferProtocol:
         self._comSend.send(paramsDictionary, self._connectionParams['Server IP'],
                            int(self._connectionParams['Server Port']),
                            HEADERSIZE=10)
+        message = Logs.LogMessaging.createLogSendNegociate(self._connectionParams['Server IP'],
+                                                       self._connectionParams['Server Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+
 
     def sendOT(self, data):
+
         self._comSend.send(data, self._connectionParams['Server IP'], int(self._connectionParams['Server Port']),
                            HEADERSIZE=10)
+        message = Logs.LogMessaging.createLogSendOT(self._connectionParams['Server IP'],
+                                                           self._connectionParams['Server Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
 
     def sendPRFKey(self, key):
+
         self._comSend.send(key, self._connectionParams['Server IP'], int(self._connectionParams['Server Port']),
                            HEADERSIZE=10)
+        message = Logs.LogMessaging.createLogSendPRFKey(self._connectionParams['Server IP'],
+                                                    self._connectionParams['Server Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
 
     def sendPsiValues(self, data):
+
         self._comSend.send(data, self._connectionParams['Client IP'], int(self._connectionParams['Client Port']),
                            HEADERSIZE=100)
+        message = Logs.LogMessaging.createLogSendPsiValues(self._connectionParams['Client IP'],
+                                                        self._connectionParams['Client Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
 
+# -------------------------------------------------------------
 
     def receiveInitiateConnection(self):
-        return self._comReceive.receive(self._connectionParams['Server IP'], int(self._connectionParams['Server Port']),
+        result= self._comReceive.receive(self._connectionParams['Server IP'], int(self._connectionParams['Server Port']),
                                         HEADERSIZE=10, flag='NoAES')
+        message = Logs.LogMessaging.createLogReceiveInitConnection(self._connectionParams['Client IP'],
+                                                           self._connectionParams['Client Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+        return result
 
 
     def receiveConfirmationInitiateConnection(self):
-        return self._comReceive.receive(self._connectionParams['Client IP'],
+        result= self._comReceive.receive(self._connectionParams['Client IP'],
                                         int(self._connectionParams['Client Port']),
                                         HEADERSIZE=10, flag='NoAES')
+        message = Logs.LogMessaging.createLogReceiveConfirmConnection(self._connectionParams['Server IP'],
+                                                           self._connectionParams['Server Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+        return result
 
     def receiveNegotiateParameters(self):
-        return self._comReceive.receive(self._connectionParams['Server IP'], int(self._connectionParams['Server Port']),
+        result= self._comReceive.receive(self._connectionParams['Server IP'], int(self._connectionParams['Server Port']),
                                         HEADERSIZE=10)
+        message = Logs.LogMessaging.createLogReceiveNegParameters(self._connectionParams['Client IP'],
+                                                           self._connectionParams['Client Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+        return result
+
 
     def receiveOT(self):
-        return self._comReceive.receive(self._connectionParams['Server IP'], int(self._connectionParams['Server Port']),
+        result= self._comReceive.receive(self._connectionParams['Server IP'], int(self._connectionParams['Server Port']),
                                         HEADERSIZE=10)
+        message = Logs.LogMessaging.createLogReceiveOT(self._connectionParams['Client IP'],
+                                                           self._connectionParams['Client Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+        return result
 
     def receiveKey(self):
-        return self._comReceive.receive(self._connectionParams['Server IP'], int(self._connectionParams['Server Port']),
+        result= self._comReceive.receive(self._connectionParams['Server IP'], int(self._connectionParams['Server Port']),
                                         HEADERSIZE=10)
+        message = Logs.LogMessaging.createLogReceivePRFKey(self._connectionParams['Client IP'],
+                                                           self._connectionParams['Client Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+        return result
 
     def receivePsiValues(self):
-        return self._comReceive.receive(self._connectionParams['Client IP'], int(self._connectionParams['Client Port']),
+        result= self._comReceive.receive(self._connectionParams['Client IP'], int(self._connectionParams['Client Port']),
                                         HEADERSIZE=100)
+        message = Logs.LogMessaging.createLogReceivePsi(self._connectionParams['Server IP'],
+                                                           self._connectionParams['Server Port'])
+        with aspectlib.weave(self.processMessage, self.log_results):
+            self.processMessage(message)
+        return result
 
---
+++ Logs/LogMessaging.py
@@ -0,0 +1,162 @@
+from datetime import datetime
+
+
+def createLogInitiateMessage(ip, port):
+    infoType = '[INFO]'
+    entity = 'RECEIVER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "Connected to SENDER (%s %s)"%(ip, str(port))
+    return infoType, entity, date, description
+
+def createLogSendConfirmationMessage(ip, port, ipS, portS):
+    infoType = '[INFO]'
+    entity = 'SENDER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "Received connection from RECEIVER (%s %s) at (%s %s)" % (ip, str(port), ipS, str(portS))
+    return infoType, entity, date, description
+
+def createLogSendReceiverRSA(ip, port):
+    infoType = '[INFO]'
+    entity = 'RECEIVER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "RSA key sent by RECEIVER to (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogReceiveReceiverRSA(ip, port):
+    infoType = '[INFO]'
+    entity = 'SENDER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "RSA key received by SENDER from (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogSendSenderRSA(ip, port):
+    infoType = '[INFO]'
+    entity = 'SENDER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "RSA key sent by SENDER to (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogReceiveSenderRSA(ip, port):
+    infoType = '[INFO]'
+    entity = 'RECEIVER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "RSA key received by RECEIVER from (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+
+def createLogSendIV(ip, port):
+    infoType = '[INFO]'
+    entity = 'RECEIVER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "IV sent by RECEIVER to (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogReceiveIV(ip, port):
+    infoType = '[INFO]'
+    entity = 'SENDER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "IV received by SENDER from (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogSendKey(ip, port):
+    infoType = '[INFO]'
+    entity = 'SENDER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "AES KEY sent by SENDER to (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogReceiveKey(ip, port):
+    infoType = '[INFO]'
+    entity = 'RECEIVER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "AES KEY received by RECEIVER from (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogSendNegociate(ip, port):
+    infoType = '[INFO]'
+    entity = 'RECEIVER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "Parameters sent to SENDER (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogSendOT(ip, port):
+    infoType = '[INFO]'
+    entity = 'RECEIVER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "OT Content sent to SENDER (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogSendPRFKey(ip, port):
+    infoType = '[INFO]'
+    entity = 'RECEIVER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "PRF Key sent to SENDER (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogSendPsiValues(ip, port):
+    infoType = '[INFO]'
+    entity = 'SENDER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "Psi Values sent to RECEIVER (%s %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogReceiveInitConnection(ip, port):
+    infoType = '[INFO]'
+    entity = 'SENDER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "Received connection from RECEIVER (%s, %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogReceiveNegParameters(ip, port):
+    infoType = '[INFO]'
+    entity = 'SENDER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "Received parameters from RECEIVER (%s, %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogReceiveOT(ip, port):
+    infoType = '[INFO]'
+    entity = 'SENDER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "Received OT data from RECEIVER (%s, %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogReceivePRFKey(ip, port):
+    infoType = '[INFO]'
+    entity = 'SENDER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "Received PRF key data from RECEIVER (%s, %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogReceivePsi(ip, port):
+    infoType = '[INFO]'
+    entity = 'RECEIVER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "Received PSI data from SENDER (%s, %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+def createLogReceiveConfirmConnection(ip, port):
+    infoType = '[INFO]'
+    entity = 'RECEIVER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = "Received confirmation from SENDER (%s, %s)" % (ip, str(port))
+    return infoType, entity, date, description
+
+
+
+
+# -----------------------------------------
+
+def exceptionReceiver(e):
+    infoType = '[ERROR]'
+    entity = 'RECEIVER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = str(e)
+    return infoType, entity, date, description
+
+def exceptionSender(e):
+    infoType = '[ERROR]'
+    entity = 'SENDER'
+    date = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    description = str(e)
+    return infoType, entity, date, description
