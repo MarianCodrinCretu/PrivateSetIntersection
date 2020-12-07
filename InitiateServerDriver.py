ecbfa973249b1449c78c14eda158f55c4d678332
Cretu Marian-Codrin <cretu.marian.codrin@gmail.com>
2020-12-07 09:59:40 +0200

    Added initiate communication module with monitor

---
+++ InitiateServerDriver.py
@@ -0,0 +1,19 @@
+import Constants
+import time
+
+from CommunicationService.ComReceive import ComReceive
+from CommunicationService.ComSend import ComSend
+from CommunicationService.SocketPool import SocketPool
+from CommunicationService.TransferProtocol import TransferProtocol
+from InitiateCommunication.InitiateCommunication import InitiateCommunication
+
+comSend = ComSend(SocketPool(20), "Thats my Kung Fu", "ABCDE FG HIJK LM")
+comReceive = ComReceive(SocketPool(20), "Thats my Kung Fu", "ABCDE FG HIJK LM")
+
+transferProtocol = TransferProtocol({'Server IP': Constants.SENDER_ADDRESS, 'Server Port': Constants.SENDER_PORT, 'Client IP':Constants.RECEIVER_ADDRESS, 'Client Port':Constants.RECEIVER_PORT}
+                                    , comSend, comReceive)
+
+initiateCommunication = InitiateCommunication(transferProtocol)
+print(initiateCommunication.receiveInitiateConnection())
+
+initiateCommunication.sendConfirmationInitiateConnection()
