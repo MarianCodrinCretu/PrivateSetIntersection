d79043321747332cf7d2a0f4888e425e97bd3724
Cretu Marian-Codrin <cretu.marian.codrin@gmail.com>
2020-12-13 19:26:04 +0200

    Decoupling OPRF Evaluation

--- OPRFEvaluation/OPRFEvaluation.py
+++ OPRFEvaluation/OPRFEvaluation.py
@@ -6,32 +6,26 @@ import CommunicationService.TransferProtocol
 class OPRFEvaluation:
 
     def __init__(self, transferProtocol : CommunicationService.TransferProtocol,
-                 dictParameters,
-                 dictFunctions,
-                 data,
-                 matrix):
+                 dictFunctions):
 
         self.transferProtocol = transferProtocol
-        self.dictParameters = dictParameters
         self.dictFunctions=dictFunctions
-        self.data=data,
         #matrix = A, if entity=Receiver, else matrix=B for entity=Sender
-        self.matrix=matrix
 
-    def computeHash1(self, x):
-        return self.dictFunctions[self.dictParameters['hash1']](x)
+    def computeHash1(self, x, dictParameters):
+        return self.dictFunctions[dictParameters['hash1']](x)
 
-    def computeHash2(self, x):
-        return self.dictFunctions[self.dictParameters['hash2']](x)
+    def computeHash2(self, x, dictParameters):
+        return self.dictFunctions[dictParameters['hash2']](x)
 
-    def computePRF(self, x, key):
-        return self.dictFunctions['FK'](x, key, self.dictParameters['l1'],
-                                        self.dictParameters['w'],
-                                        self.dictParameters['m'],
-                                        self.dictParameters['prf'])
+    def computePRF(self, x, key, dictParameters):
+        return self.dictFunctions['FK'](x, key, dictParameters['l1'],
+                                        dictParameters['w'],
+                                        dictParameters['m'],
+                                        dictParameters['prf'])
 
-    def generateKey(self):
-        lambdaP = self.dictParameters['lambda']
+    def generateKey(self, dictParameters):
+        lambdaP = dictParameters['lambda']
         key = ""
         for counter in range(lambdaP):
             key+= str(random.randint(0,1))
@@ -46,22 +40,22 @@ class OPRFEvaluation:
         # here, key is a string
         return self.transferProtocol.receiveKey()
 
-    def generateSenderPsiValues(self, key):
+    def generateSenderPsiValues(self, key, matrix, data, dictParameters):
         psiSenderList = []
 
         # self.matrix = C (for sender)
-        for x in self.data:
-            v = self.computePRF(self.computeHash1(x), key)
+        for x in data:
+            v = self.computePRF(self.computeHash1(x, dictParameters), key, dictParameters)
 
             psiDigest = ""
 
-            for counter in range(self.dictParameters['w']):
-                intermediary = self.matrix[v[counter]][counter]
+            for counter in range(dictParameters['w']):
+                intermediary = matrix[v[counter]][counter]
 
                 #to see in which form the psiDigest is computed
                 psiDigest += str(intermediary)
 
-            psiHash = self.computeHash2(psiDigest)
+            psiHash = self.computeHash2(psiDigest, dictParameters)
             psiSenderList.append(psiHash)
 
         return psiSenderList
@@ -74,22 +68,22 @@ class OPRFEvaluation:
         # receive psi values from the sender
         return self.transferProtocol.receivePsiValues()
 
-    def evaluatePsiValues(self, key, senderPsiValues):
+    def evaluatePsiValues(self, key, senderPsiValues, matrix, data, dictParameters):
         result = []
 
         #self.matrix = A (for receiver)
         #here is the data from Receiver
-        for y in self.data:
-            v = self.computePRF(self.computeHash1(y), key)
+        for y in data:
+            v = self.computePRF(self.computeHash1(y, dictParameters), key, dictParameters)
 
             psiDigest = ""
 
-            for counter in range(self.dictParameters['w']):
-                intermediary = self.matrix[v[counter]][counter]
+            for counter in range(dictParameters['w']):
+                intermediary = matrix[v[counter]][counter]
                 psiDigest += str(intermediary)
 
             # to see in which form the psiDigest is computed
-            psiHash = self.computeHash2(psiDigest)
+            psiHash = self.computeHash2(psiDigest, dictParameters)
 
             #checkingEquality
             if psiHash in senderPsiValues:
