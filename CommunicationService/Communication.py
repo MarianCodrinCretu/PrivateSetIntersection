39f362e76a12a7fe97552618f50806540caa7f11
Cretu Marian-Codrin <cretu.marian.codrin@gmail.com>
2020-12-16 18:09:27 +0200

    Retuned Communication Service

--- CommunicationService/Communication.py
+++ CommunicationService/Communication.py
@@ -1,11 +1,18 @@
 from CommunicationService.SocketPool import SocketPool
-
+import string
+import random
 
 class Communication:
 
     def __init__(self, socketPool: SocketPool, aesKey, aesIV):
 
         self._socketPool = socketPool
+        if aesKey is None or len(aesKey)!=16:
+            self.aesKey = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k = 16))
+        if aesIV is None or len(aesIV)!=16:
+            self.aesIV = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k = 16))
         self.aesKey=aesKey.encode("utf8")
         self.aesIV=aesIV.encode("utf8")
 
--- CommunicationService/SocketPool.py
+++ CommunicationService/SocketPool.py
@@ -1,11 +1,19 @@
 import socket
-import time
 
 class SocketPool:
 
     def __init__ (self, size):
 
+        if isinstance(size, int) or isinstance(size, float):
+            if int(size)<10:
+                size=10
+            else:
+                size=int(size)
+        else:
+            size=10
         self._socketPools=[socket.socket(socket.AF_INET, socket.SOCK_STREAM) for _ in range(size)]
+        for index in range(size):
+            self._socketPools[index].setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     def acquire(self):
 
@@ -13,6 +21,9 @@ class SocketPool:
 
     def release(self, reusableSocket):
         reusableSocket.close()
-        #self._socketPools.append(reusableSocket)
+        if len(self._socketPools)==0:
+            socketToAppend=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socketToAppend.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self._socketPools.append(socketToAppend)
 
 
